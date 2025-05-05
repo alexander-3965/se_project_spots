851fc02a2279f2ccb31016f6a35055ebdc25f475
Alexander Rocha <alexrochalopez@gmail.com>
2025-05-05 09:50:23 -0400

    Project 4 Final stage (adding functionality to forms)

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,21 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-profile-modal");
 const editCloseBtn = editModal.querySelector(".modal__close-btn");
+const nameInput = editModal.querySelector("#profile-name-input");
+const descriptionInput = editModal.querySelector("#profile-description-input");
+const editForm = editModal.querySelector(".modal__form");
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const NewPostFormEl = newPostModal.querySelector(".modal__form");
+const newPostLink = newPostModal.querySelector("#image-link-input");
+const newPostCaption = newPostModal.querySelector("#card-caption-input");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_is-opened");
 });
 
@@ -20,3 +30,21 @@ newPostBtn.addEventListener("click", function () {
 newCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  editModal.classList.remove("modal_is-opened");
+}
+
+editForm.addEventListener("submit", handleEditFormSubmit);
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostLink.value);
+  console.log(newPostCaption.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+NewPostFormEl.addEventListener("submit", handleNewPostFormSubmit);
